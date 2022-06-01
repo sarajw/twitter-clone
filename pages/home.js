5c4abd7720e3d58515e7bba1dde80fa4b945c40b
SJW <88535714+sarajw@users.noreply.github.com>
2022-06-01 14:46:00 +0200

    Initial commit

---
+++ pages/home.js
@@ -0,0 +1,16 @@
+import NewTweet from 'components/NewTweet'
+import { useSession } from 'next-auth/react'
+
+export default function Home() {
+  const { data: session, status } = useSession()
+
+  if (status === 'loading') {
+    return <p>Loading...</p>
+  }
+
+  return (
+    <div>
+      {session ? <NewTweet /> : <p>You are not logged in 😞</p>}
+    </div>
+  )
+}
